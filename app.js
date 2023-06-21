5a467573073d05a3c43c91ba8ab0ac52b0970a91
Josh Hancock <livingicon@gmail.com>
2023-06-21 19:55:20 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+
+app.get('/api', (req, res) => {
+  res.json({
+    message: 'Welcome to the API'
+  });
+});
+
+app.post('/api/post', (req, res) => {
+  res.json({
+    message: 'Post created...'
+  });
+});
+
+// route to get the JWS token
+app.post('/api/login', (req, res) => {
+  jwt.sign(); // can do sync or async
+})
+
+app.listen(5000, () => console.log('Server started on port 5000'));
